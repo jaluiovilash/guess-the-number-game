4ac86e2a296a8c2538ee0aab1ea70a8545597be4
jaluiovilash <ovilashjalui@gmail.com>
2024-05-29 16:11:03 +0530

    updated the script file

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
-console.log("the game has started");
+console.log("The game has started");
 const randomNum = parseInt(Math.random() * 100 + 1);
+console.log(randomNum);
 
-const subt = document.querySelector('#subt');
+const submit = document.querySelector('#subt');
 const userInput = document.querySelector('#guessField');
 const guessSlot = document.querySelector('.guesses');
 const remaining = document.querySelector('.lastResult');
@@ -15,3 +16,71 @@ let numGuess = 1;
 
 let playGame = true
 
+if (playGame) {
+    submit.addEventListener('click', (e) => {
+        e.preventDefault();
+        const guess = parseInt(userInput.value);
+        console.log(guess);
+        validateGame(guess);
+    });
+}
+
+function validateGame(guess) {
+    if (isNaN(guess)) {
+        alert('Please enter a valid number.');
+    } else if (guess < 1) {
+        alert('Please enter a valid number more than 1.');
+    } else if (guess > 100) {
+        alert('Please enter a valid number less than 100.');
+    } else {
+        prevGuess.push(guess);
+        if (numGuess === 11) {
+            displayGuess(guess);
+            displayMessage(`Game Over. Random number was: ${randomNum}`)
+            endGame();
+        } else {
+            displayGuess(guess);
+            checkGuess(guess);
+        }
+    }
+}
+
+function checkGuess(guess) {
+    if (guess === randomNum) {
+        displayMessage(`CONGRATS, you've won.`);
+        endGame();
+    } else if (guess < randomNum) {
+        displayMessage(`Number is too low.`);
+    } else if (guess > randomNum) {
+        displayMessage(`Number is too high.`);
+    }
+}
+
+// clean up method to clean the submit box and add to the previous guess list
+function displayGuess(guess) {
+    userInput.value = '';
+    guessSlot.innerHTML += `${guess}  `;
+    numGuess++;
+    remaining.innerHTML = `${11 - numGuess}`;
+}
+
+function displayMessage(message) {
+    lowOrHigh.innerHTML = `<h2>${message}</h2>`;
+}
+
+function endGame() {
+    userInput.value = '';
+    userInput.setAttribute('disabled', '');
+    p.classList.add('button');
+    p.innerHTML = `<h2 id="newGame">Start new game</h2>`;
+    startOver.appendChild(p);
+    playGame = false;
+    newGame();
+}
+
+function newGame() {
+    const newGameBtn = document.querySelector('#newGame')
+    newGameBtn.addEventListener('click', (e) => {
+        location.reload();
+    })
+}
